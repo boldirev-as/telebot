e6a726148ee157f2e3647392786a1e95737b5247
boldirev-as <60057518+boldirev-as@users.noreply.github.com>
2021-01-10 10:56:39 +0500

    add_init

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main():
         last_chat_id = last_update['message']['chat']['id']
         last_chat_name = last_update['message']['chat']['first_name']
 
-        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+        if last_chat_text.lower() in greetings:
             greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
             # today += 1
 
